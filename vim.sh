f00204324518fabd66996cd6472c2cdafc967d54
_v7__ <nuovince@gmail.com>
2016-01-22 14:30:47 +0800

    fix vim sh

--- vim.sh
+++ vim.sh
@@ -5,10 +5,10 @@ echo '==============================='
 echo 'start to install dependences...'
 case "$OSTYPE" in
     darwin*)  brew install vim git pip curl;;
-    linux*)   sudo apt-get install vim exuberant-ctags git pip curl;;
+    linux*)   apt-get install vim exuberant-ctags git pip curl;;
     *)        echo "unknown: OS: $OSTYPE, U should install dependences by yourself" ;;
 esac
-sudo pip install dbgp vim-debug pep8 flake8 pyflakes isort
+pip install dbgp vim-debug pep8 flake8 pyflakes isort
 
 echo '==============================='
 echo 'start to download vimrc file...'
@@ -16,10 +16,9 @@ cp ~/.vimrc /tmp/vimrc.bak
 curl -O https://raw.githubusercontent.com/fisadev/fisa-vim-config/master/.vimrc
 mv .vimrc ~/.vimrc
 
-mkdir ~/.vim/
-mkdir ~/.vim/dirs/
 sudo chown -R $USER ~/.vim/  
 echo '==============================='
 echo 'start to install vim plugins...'
 vim +BundleClean +BundleInstall! +qa
 sudo chown -R $USER ~/.vim/  
+vim +BundleInstall! +qa
